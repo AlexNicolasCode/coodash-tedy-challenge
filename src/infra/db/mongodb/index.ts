bf0f610c0490bd0f027eb7ae31e8d09c02818acd
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-07 12:54:19 -0300

    feat: ProductMongoRepository was created

---
+++ src/infra/db/mongodb/index.ts
@@ -0,0 +1,1 @@
+export * from "./product.mongo.repository"
---
+++ src/infra/db/mongodb/product.mongo.repository.ts
@@ -0,0 +1,14 @@
+import { GetProductsRepository } from "@/data/protocol/db";
+import { Product } from "@/domain/model";
+import { ProductEntity } from "../entity";
+
+export class ProductMongoRepository implements GetProductsRepository {
+    async get_products (page: number): Promise<Product[]> {
+        const maxPerPage = 10
+        const skipCount = maxPerPage * (page - 1)
+        return await ProductEntity
+            .find()
+            .skip(skipCount)
+            .limit(maxPerPage)
+    }
+}
