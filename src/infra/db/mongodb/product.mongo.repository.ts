57beeafb5f053707c956e947bd84297ea60d6fbc
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-07 14:33:53 -0300

    feat: ProductMongoRepository implements GetProductRepository and should be able to getProductByCode

--- src/infra/db/mongodb/product.mongo.repository.ts
+++ src/infra/db/mongodb/product.mongo.repository.ts
@@ -1,8 +1,8 @@
-import { GetProductsRepository } from "@/data/protocol/db";
+import { GetProductRepository, GetProductsRepository } from "@/data/protocol/db";
 import { Product } from "@/domain/model";
 import { ProductEntity } from "../entity";
 
-export class ProductMongoRepository implements GetProductsRepository {
+export class ProductMongoRepository implements GetProductsRepository, GetProductRepository {
     async getProducts (page: number): Promise<Product[]> {
         const maxPerPage = 10
         const skipCount = maxPerPage * (page - 1)
@@ -10,5 +10,9 @@ export class ProductMongoRepository implements GetProductsRepository {
             .find()
             .skip(skipCount)
             .limit(maxPerPage)
+    }
+
+    async getProduct (code: number): Promise<Product> {
+        return await ProductEntity.findOne({ code })
     }
 }
