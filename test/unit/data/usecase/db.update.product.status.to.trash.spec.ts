0ad3f7c0c1c2130eef4475ccac6af416b92e9f59
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-07 15:05:33 -0300

    test: DbUpdateProductStatusToTrash should throw if UpdateProductStatusRepositorySpy throws

--- test/unit/data/usecase/db.update.product.status.to.trash.spec.ts
+++ test/unit/data/usecase/db.update.product.status.to.trash.spec.ts
@@ -2,6 +2,7 @@ import { faker } from "@faker-js/faker"
 
 import { DbUpdateProductStatusToTrash } from "@/data/usecase"
 import { UpdateProductStatusRepositorySpy } from "../mock"
+import { throwError } from "test/unit/domain/helper"
 
 describe('DbUpdateProductStatusToTrash', () => {
     test('should call UpdateProductStatusRepositorySpy with correct params', async () => {
@@ -15,5 +16,16 @@ describe('DbUpdateProductStatusToTrash', () => {
             code: fakeCode,
             status: 'trash'
         })
+    })
+    
+    test('should throw if UpdateProductStatusRepositorySpy throws', async () => {
+        const updateProductStatusRepositorySpy = new UpdateProductStatusRepositorySpy()
+        const sut = new DbUpdateProductStatusToTrash(updateProductStatusRepositorySpy)
+        const fakePage = faker.number.int()
+        jest.spyOn(updateProductStatusRepositorySpy, 'updateStatus').mockImplementationOnce(throwError)
+        
+        const promise = sut.changeStatusToTrash(fakePage)
+
+        await expect(promise).rejects.toThrow()
     })
 })
