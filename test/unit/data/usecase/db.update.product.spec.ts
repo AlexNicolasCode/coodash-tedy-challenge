f36b084aba7dcc4e18c635f311eec23fb9587282
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-07 15:52:15 -0300

    test: DbUpdateProduct should throw if UpdateProductRepository throws

--- test/unit/data/usecase/db.update.product.spec.ts
+++ test/unit/data/usecase/db.update.product.spec.ts
@@ -1,13 +1,15 @@
 import { faker } from "@faker-js/faker"
 
-import { UpdateProductStatusRepositorySpy } from "../mock"
+import { UpdateProductRepositorySpy, UpdateProductStatusRepositorySpy } from "../mock"
 import { throwError } from "test/unit/domain/helper"
 import { DbUpdateProduct } from "@/data/usecase"
+import { mockProduct } from "test/unit/domain/mock"
 
 describe('DbUpdateProduct', () => {
     test('should call UpdateProductStatusRepositorySpy with correct params', async () => {
+        const updateProductRepositorySpy = new UpdateProductRepositorySpy()
         const updateProductStatusRepositorySpy = new UpdateProductStatusRepositorySpy()
-        const sut = new DbUpdateProduct(updateProductStatusRepositorySpy)
+        const sut = new DbUpdateProduct(updateProductRepositorySpy, updateProductStatusRepositorySpy)
         const fakeCode = faker.number.int()
         
         await sut.updateStatusToTrash(fakeCode)
@@ -19,12 +21,28 @@ describe('DbUpdateProduct', () => {
     })
     
     test('should throw if UpdateProductStatusRepositorySpy throws', async () => {
+        const updateProductRepositorySpy = new UpdateProductRepositorySpy()
         const updateProductStatusRepositorySpy = new UpdateProductStatusRepositorySpy()
-        const sut = new DbUpdateProduct(updateProductStatusRepositorySpy)
+        const sut = new DbUpdateProduct(updateProductRepositorySpy, updateProductStatusRepositorySpy)
         const fakePage = faker.number.int()
         jest.spyOn(updateProductStatusRepositorySpy, 'updateStatus').mockImplementationOnce(throwError)
         
         const promise = sut.updateStatusToTrash(fakePage)
+
+        await expect(promise).rejects.toThrow()
+    })
+    
+    test('should throw if UpdateProductRepository throws', async () => {
+        const updateProductRepositorySpy = new UpdateProductRepositorySpy()
+        const updateProductStatusRepositorySpy = new UpdateProductStatusRepositorySpy()
+        const sut = new DbUpdateProduct(updateProductRepositorySpy, updateProductStatusRepositorySpy)
+        const fakeParams = {
+            code: faker.number.int(),
+            product: mockProduct()
+        }
+        jest.spyOn(updateProductRepositorySpy, 'update').mockImplementationOnce(throwError)
+        
+        const promise = sut.update(fakeParams)
 
         await expect(promise).rejects.toThrow()
     })
