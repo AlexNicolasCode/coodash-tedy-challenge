198eeac3100f5e954b357181d45e2ffcff27f061
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-07 13:26:52 -0300

    feat: serverError http helper was created

--- src/presentation/helper/http.helper.ts
+++ src/presentation/helper/http.helper.ts
@@ -4,3 +4,8 @@ export const ok = <T>(data: T): HttpResponse<T> => ({
     statusCode: 200,
     body: data
 })
+
+export const serverError = (): HttpResponse<null>  => ({
+    statusCode: 500,
+    body: null
+})
