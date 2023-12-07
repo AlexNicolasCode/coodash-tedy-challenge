7fc132ae0c112eda2ffefc0c2fe8dc118ef523ce
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-07 16:23:53 -0300

    fix: set DbUpdateProduct params from factory to enable correct build

--- src/main/factory/usecase/update.product.status.to.trash.ts
+++ src/main/factory/usecase/update.product.status.to.trash.ts
@@ -3,6 +3,10 @@ import { UpdateProductStatusToTrash } from "@/domain/usecase";
 import { ProductMongoRepository } from "@/infra/db/mongodb";
 
 export const makeUpdateProductStatusToTrash = (): UpdateProductStatusToTrash => {
+    const updateProductRepository = new ProductMongoRepository()
     const updateProductStatusToTrashRepository = new ProductMongoRepository()
-    return new DbUpdateProduct(updateProductStatusToTrashRepository)
+    return new DbUpdateProduct(
+        updateProductRepository,
+        updateProductStatusToTrashRepository
+    )
 }
