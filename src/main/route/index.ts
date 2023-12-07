3489f4edb3f0cf2a74167faf4050ff901339a925
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-07 15:35:15 -0300

    feat: DELETE /products/:code endpoint was created

--- src/main/route/index.ts
+++ src/main/route/index.ts
@@ -2,9 +2,14 @@
 import { Router } from "express"
 
 import { adaptRoute } from "@/main/adapter"
-import { makeGetProductsController, makeGetProductController } from "@/main/factory/controller"
+import {
+  makeGetProductsController,
+  makeGetProductController,
+  makeUpdateProductStatusToTrashController,
+} from "@/main/factory/controller"
 
 export default (router: Router): void => {
   router.get("/products", adaptRoute(makeGetProductsController()))
   router.get("/products/:code", adaptRoute(makeGetProductController()))
+  router.delete("/products/:code", adaptRoute(makeUpdateProductStatusToTrashController()))
 }
