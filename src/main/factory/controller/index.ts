298da6eb8b5e628d115afb62e0a45ff40a6fe489
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-07 16:35:12 -0300

    refactor: update file name to improve architecture redability

--- src/main/factory/controller/index.ts
+++ src/main/factory/controller/index.ts
@@ -1,4 +1,4 @@
 export * from "./get.product.controller"
 export * from "./get.products.controller"
 export * from "./update.product.controller"
-export * from "./update.product.status.to.trash"
+export * from "./update.product.status.to.trash.controller"
