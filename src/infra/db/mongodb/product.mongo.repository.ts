34351838bf460fc9ebb47e917b6bd7f62e112dbc
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-07 15:19:44 -0300

    feat: ProductMongoRepository implements UpdateProductStatusRepository and should be able to update product status

--- src/infra/db/mongodb/product.mongo.repository.ts
+++ src/infra/db/mongodb/product.mongo.repository.ts
@@ -1,8 +1,8 @@
-import { GetProductRepository, GetProductsRepository } from "@/data/protocol/db";
+import { GetProductRepository, GetProductsRepository, UpdateProductStatusRepository } from "@/data/protocol/db";
 import { Product } from "@/domain/model";
 import { ProductEntity } from "../entity";
 
-export class ProductMongoRepository implements GetProductsRepository, GetProductRepository {
+export class ProductMongoRepository implements GetProductsRepository, GetProductRepository, UpdateProductStatusRepository {
     async getProducts (page: number): Promise<Product[]> {
         const maxPerPage = 10
         const skipCount = maxPerPage * (page - 1)
@@ -14,5 +14,13 @@ export class ProductMongoRepository implements GetProductsRepository, GetProduct
 
     async getProduct (code: number): Promise<Product> {
         return await ProductEntity.findOne({ code })
+    }
+
+    async updateStatus (params: UpdateProductStatusRepository.Request): Promise<void> {
+        await ProductEntity.updateOne({
+            code: params.code
+        }, {
+            status: params.status
+        })
     }
 }
