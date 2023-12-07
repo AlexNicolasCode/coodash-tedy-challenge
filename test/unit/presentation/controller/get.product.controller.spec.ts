89752e2f3b844be36141693d7f246f8130069070
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-07 14:41:22 -0300

    test: GetProductController should return 200 on success

--- test/unit/presentation/controller/get.product.controller.spec.ts
+++ test/unit/presentation/controller/get.product.controller.spec.ts
@@ -2,6 +2,7 @@ import { faker } from "@faker-js/faker"
 
 import { GetProductController } from "@/presentation/controller"
 import { GetProductSpy } from "../mock"
+import { ok } from "@/presentation/helper"
 
 const mockRequest = (): GetProductController.Request => ({
     params: {
@@ -28,5 +29,15 @@ describe('GetProductController', () => {
         const products = await sut.handle(request)
         
         expect(products.body).toStrictEqual(getProductSpy.result)
+    })
+
+    test('should return 200 on success', async () => {
+        const getProductSpy = new GetProductSpy()
+        const sut = new GetProductController(getProductSpy)
+        const request = mockRequest()
+        
+        const products = await sut.handle(request)
+        
+        expect(products.statusCode).toStrictEqual(ok(getProductSpy.result).statusCode)
     })
 })
