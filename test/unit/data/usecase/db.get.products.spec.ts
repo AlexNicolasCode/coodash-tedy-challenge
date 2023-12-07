4927d74473f7351f6a959b97bfb6eb80b120bd88
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-07 10:07:31 -0300

    test: DbGetProducts should throw if GetProductsRepositorySpy throws

--- test/unit/data/usecase/db.get.products.spec.ts
+++ test/unit/data/usecase/db.get.products.spec.ts
@@ -2,6 +2,7 @@ import { faker } from "@faker-js/faker"
 
 import { DbGetProducts } from "@/data/usecase"
 import { GetProductsRepositorySpy } from "../mock"
+import { throwError } from "test/unit/domain/helper"
 
 describe('DbGetProducts', () => {
     test('should call GetProductsRepositorySpy with correct page', async () => {
@@ -12,5 +13,16 @@ describe('DbGetProducts', () => {
         await sut.get_products(fakePage)
 
         expect(getProductsRepositorySpy.page).toStrictEqual(fakePage)
+    })
+
+    test('should throw if GetProductsRepositorySpy throws', async () => {
+        const getProductsRepositorySpy = new GetProductsRepositorySpy()
+        const sut = new DbGetProducts(getProductsRepositorySpy)
+        const fakePage = faker.number.int()
+        jest.spyOn(getProductsRepositorySpy, 'get_products').mockImplementationOnce(throwError)
+        
+        const promise = sut.get_products(fakePage)
+
+        await expect(promise).rejects.toThrow()
     })
 })
