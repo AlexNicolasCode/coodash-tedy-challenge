71261bf7109005b3a154da7305ce1dfd3f537df0
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-07 14:30:31 -0300

    test: ProductMongoRepository, when getProduct method, should return correct product on success

--- test/integration/infra/db/product.mongo.repository.spec.ts
+++ test/integration/infra/db/product.mongo.repository.spec.ts
@@ -23,51 +23,64 @@ describe('ProductMongoRepository', () => {
         await mongoDb.stop()
     })
 
-    test('should return correct products on success', async () => {
-        const fakeProduct = await mockProductEntity()
-        const sut = new ProductMongoRepository()
-        const fakePage = 1
-        
-        const products = await sut.getProducts(fakePage)
-
-        expect(products.some((product) => product.code === fakeProduct.code)).toBe(true)
+    describe('getProducts', () => {
+        test('should return correct products on success', async () => {
+            const fakeProduct = await mockProductEntity()
+            const sut = new ProductMongoRepository()
+            const fakePage = 1
+            
+            const products = await sut.getProducts(fakePage)
+    
+            expect(products.some((product) => product.code === fakeProduct.code)).toBe(true)
+        })
+    
+        test('should return only 10 products by request', async () => {
+            const productEntityLenght = faker.number.int({ min: 10, max: 20 }) 
+            await mockProductEntityList(productEntityLenght)
+            const sut = new ProductMongoRepository()
+            const fakePage = 1
+            const maxProductsPerPage = 10
+            
+            const products = await sut.getProducts(fakePage)
+    
+            expect(products.length).toBe(maxProductsPerPage)
+        })
+    
+        test('should return different products when page selected change', async () => {
+            const productEntityLenght = faker.number.int({ min: 11, max: 19 }) 
+            await mockProductEntityList(productEntityLenght)
+            const sut = new ProductMongoRepository()
+            
+            const productsFromFirstQuery = await sut.getProducts(1)
+            const productsFromSecondQuery = await sut.getProducts(2)
+    
+            expect(productsFromFirstQuery.some((product) => productsFromSecondQuery.includes(product))).toBe(false)
+        })
+    
+        test('should return correctly last page lenght', async () => {
+            const productEntityLenght = faker.number.int({ min: 11, max: 100 }) 
+            await mockProductEntityList(productEntityLenght)
+            const sut = new ProductMongoRepository()
+            const maxProductsPerPage = 10
+            const fakePage = Math.ceil(productEntityLenght / maxProductsPerPage)
+            const lastPageLenght = (productEntityLenght % maxProductsPerPage) !== 0 
+                ? (productEntityLenght % maxProductsPerPage)
+                : maxProductsPerPage 
+            
+            const products = await sut.getProducts(fakePage)
+    
+            expect(products.length).toBe(lastPageLenght)
+        })
     })
 
-    test('should return only 10 products by request', async () => {
-        const productEntityLenght = faker.number.int({ min: 10, max: 20 }) 
-        await mockProductEntityList(productEntityLenght)
-        const sut = new ProductMongoRepository()
-        const fakePage = 1
-        const maxProductsPerPage = 10
-        
-        const products = await sut.getProducts(fakePage)
-
-        expect(products.length).toBe(maxProductsPerPage)
-    })
-
-    test('should return different products when page selected change', async () => {
-        const productEntityLenght = faker.number.int({ min: 11, max: 19 }) 
-        await mockProductEntityList(productEntityLenght)
-        const sut = new ProductMongoRepository()
-        
-        const productsFromFirstQuery = await sut.getProducts(1)
-        const productsFromSecondQuery = await sut.getProducts(2)
-
-        expect(productsFromFirstQuery.some((product) => productsFromSecondQuery.includes(product))).toBe(false)
-    })
-
-    test('should return correctly last page lenght', async () => {
-        const productEntityLenght = faker.number.int({ min: 11, max: 100 }) 
-        await mockProductEntityList(productEntityLenght)
-        const sut = new ProductMongoRepository()
-        const maxProductsPerPage = 10
-        const fakePage = Math.ceil(productEntityLenght / maxProductsPerPage)
-        const lastPageLenght = (productEntityLenght % maxProductsPerPage) !== 0 
-            ? (productEntityLenght % maxProductsPerPage)
-            : maxProductsPerPage 
-        
-        const products = await sut.getProducts(fakePage)
-
-        expect(products.length).toBe(lastPageLenght)
+    describe('getProduct', () => {
+        test('should return correct product on success', async () => {
+            const fakeProduct = await mockProductEntity()
+            const sut = new ProductMongoRepository()
+            
+            const product = await sut.getProduct(fakeProduct.code)
+    
+            expect(product.code).toBe(fakeProduct.code)
+        })
     })
 })
