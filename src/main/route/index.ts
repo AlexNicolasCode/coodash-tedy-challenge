866946e7ec757ec9b7375a26269f5f3770c95360
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-07 16:36:00 -0300

    feat: PUT /products/:code endpoint was created

--- src/main/route/index.ts
+++ src/main/route/index.ts
@@ -6,10 +6,12 @@ import {
   makeGetProductsController,
   makeGetProductController,
   makeUpdateProductStatusToTrashController,
+  makeUpdateProductController,
 } from "@/main/factory/controller"
 
 export default (router: Router): void => {
   router.get("/products", adaptRoute(makeGetProductsController()))
   router.get("/products/:code", adaptRoute(makeGetProductController()))
+  router.put("/products/:code", adaptRoute(makeUpdateProductController()))
   router.delete("/products/:code", adaptRoute(makeUpdateProductStatusToTrashController()))
 }
