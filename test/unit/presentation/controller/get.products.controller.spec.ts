163ed4d06ed8497ad24117c7d1c0471fccd94f84
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-07 13:22:34 -0300

    test: GetProductsController should call GetProducts with correct page

---
+++ test/unit/presentation/controller/get.products.controller.spec.ts
@@ -0,0 +1,22 @@
+import { faker } from "@faker-js/faker"
+
+import { GetProductsController } from "@/presentation/controller"
+import { GetProductsSpy } from "../mock"
+
+const mockRequest = (): GetProductsController.Request => ({
+    params: {
+        page: faker.number.int()
+    }   
+})
+
+describe('GetProductsController', () => {
+    test('should call GetProducts with correct page', async () => {
+        const getProductsSpy = new GetProductsSpy()
+        const sut = new GetProductsController(getProductsSpy)
+        const request = mockRequest()
+        
+        await sut.handle(request)
+        
+        expect(getProductsSpy.page).toStrictEqual(request.params.page)
+    })
+})
