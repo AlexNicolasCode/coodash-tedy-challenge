a29554bb475db8b29fbf51f7066d54e686e650cf
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-07 15:31:43 -0300

    test: UpdateProductStatusToTrashController should return 204 on success

--- test/unit/presentation/controller/update.product.status.to.trash.controller.spec.ts
+++ test/unit/presentation/controller/update.product.status.to.trash.controller.spec.ts
@@ -2,6 +2,7 @@ import { faker } from "@faker-js/faker"
 
 import { UpdateProductStatusToTrashController } from "@/presentation/controller"
 import { UpdateProductStatusToTrashSpy } from "../mock"
+import { noContent } from "@/presentation/helper"
 
 const mockRequest = (): UpdateProductStatusToTrashController.Request => ({
     params: {
@@ -18,5 +19,16 @@ describe('UpdateProductStatusToTrashController', () => {
         await sut.handle(request)
         
         expect(updateProductStatusToTrashSpy.code).toStrictEqual(request.params.code)
+    })
+
+    
+    test('should return 204 on success', async () => {
+        const updateProductStatusToTrashSpy = new UpdateProductStatusToTrashSpy()
+        const sut = new UpdateProductStatusToTrashController(updateProductStatusToTrashSpy)
+        const request = mockRequest()
+        
+        const response = await sut.handle(request)
+        
+        expect(response).toStrictEqual(noContent())
     })
 })
