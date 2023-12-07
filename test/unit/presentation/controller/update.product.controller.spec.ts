be7e7d2ba009109f589e7a2f33887023094861d1
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-07 16:31:19 -0300

    test: UpdateProductController should return correct product on success

--- test/unit/presentation/controller/update.product.controller.spec.ts
+++ test/unit/presentation/controller/update.product.controller.spec.ts
@@ -3,6 +3,7 @@ import { faker } from "@faker-js/faker"
 import { UpdateProductController } from "@/presentation/controller"
 import {UpdateProductSpy } from "../mock"
 import { mockProduct } from "test/unit/domain/mock"
+import { ok } from "@/presentation/helper"
 
 const mockRequest = (): UpdateProductController.Request => ({
     params: {
@@ -23,5 +24,15 @@ describe('UpdateProductController', () => {
             code: request.params.code,
             product: request.body
         })
+    })
+
+    test('should return correct product on success', async () => {
+        const updateProductSpy = new UpdateProductSpy()
+        const sut = new UpdateProductController(updateProductSpy)
+        const request = mockRequest()
+        
+        const product = await sut.handle(request)
+        
+        expect(product).toStrictEqual(ok(updateProductSpy.result))
     })
 })
