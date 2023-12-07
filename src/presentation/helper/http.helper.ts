1e7bd94db31cd02db9c1b2ac395e265ba64738ac
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-07 14:44:44 -0300

    feat: notFound http helper was created

--- src/presentation/helper/http.helper.ts
+++ src/presentation/helper/http.helper.ts
@@ -5,6 +5,11 @@ export const ok = <T>(data: T): HttpResponse<T> => ({
     body: data
 })
 
+export const notFound = (): HttpResponse<null>  => ({
+    statusCode: 404,
+    body: null
+})
+
 export const serverError = (): HttpResponse<null>  => ({
     statusCode: 500,
     body: null
