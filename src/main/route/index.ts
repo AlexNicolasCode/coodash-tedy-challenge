b33237f7e13f8d482c16742c4928c663ef3ddd2a
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-07 14:47:34 -0300

    feat: GET /products/:code endpoint was created

--- src/main/route/index.ts
+++ src/main/route/index.ts
@@ -2,8 +2,9 @@
 import { Router } from "express"
 
 import { adaptRoute } from "@/main/adapter"
-import { makeGetProductsController } from "@/main/factory/controller"
+import { makeGetProductsController, makeGetProductController } from "@/main/factory/controller"
 
 export default (router: Router): void => {
   router.get("/products", adaptRoute(makeGetProductsController()))
+  router.get("/products/:code", adaptRoute(makeGetProductController()))
 }
