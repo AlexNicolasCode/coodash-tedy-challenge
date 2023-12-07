bebd5a7b02652a5df903fe3edd38ae6b4bd3a3d8
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-07 15:56:39 -0300

    feat: DbUpdateProduct implements UpdateProduct and should be able to update product

--- src/data/usecase/db.update.product.ts
+++ src/data/usecase/db.update.product.ts
@@ -1,10 +1,16 @@
-import { UpdateProductStatusToTrash } from "@/domain/usecase";
-import { UpdateProductStatusRepository } from "../protocol/db";
+import { UpdateProduct, UpdateProductStatusToTrash } from "@/domain/usecase";
+import { UpdateProductRepository, UpdateProductStatusRepository } from "../protocol/db";
+import { Product } from "@/domain/model";
 
-export class DbUpdateProduct implements UpdateProductStatusToTrash {
+export class DbUpdateProduct implements UpdateProductStatusToTrash, UpdateProduct {
     constructor (
+        private readonly updateProductRepository: UpdateProductRepository,
         private readonly updateProductStatusRepository: UpdateProductStatusRepository
     ) {}
+
+    async update ({ code, product }: UpdateProduct.Request): Promise<Product> {
+        return this.updateProductRepository.update({ code, product })
+    }
 
     async updateStatusToTrash (code: number): Promise<void> {
         await this.updateProductStatusRepository.updateStatus({
