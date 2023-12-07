6496217b1fc199b4df3058cdd140ee2032190775
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-07 11:24:45 -0300

    feat: change ProductEntity mock to enable return content inserted in database and compare with another contents

--- test/integration/infra/mock/product.ts
+++ test/integration/infra/mock/product.ts
@@ -1,6 +1,7 @@
+import { Product } from "@/domain/model"
 import { ProductEntity } from "@/infra/db/entity"
 import { mockProduct } from "test/unit/domain/mock"
 
-export const mockProductEntity = async (): Promise<void> => {
-    await ProductEntity.create(mockProduct())
+export const mockProductEntity = async (): Promise<Product> => {
+    return await ProductEntity.create(mockProduct())
 }
