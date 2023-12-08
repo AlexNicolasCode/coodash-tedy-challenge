ba41799e5b04a1d0d9fa608e53ba30ffc5f7ba9f
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-08 17:13:45 -0300

    feat: all routines should be runned using America/Sao_paulo timezone to enable developer config correctly time

--- src/main/adapter/corn.routine.adapter.ts
+++ src/main/adapter/corn.routine.adapter.ts
@@ -3,5 +3,8 @@ import cron from "node-cron"
 import { Routine } from "@/routine/protocol"
 
 export const adaptRoutine = (time: string, routine: Routine) => {
-    cron.schedule(time, routine.handle).start()
+    cron.schedule(time, routine.handle, {
+        scheduled: true,
+        timezone: "America/Sao_Paulo"
+    })
 }
