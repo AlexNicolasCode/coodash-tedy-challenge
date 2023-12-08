80b3dbff0be1b43fd713156ba23641f9f3e79dab
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-08 16:00:20 -0300

    test: RemoteGetProductSeeds should call SetFileStatusRepository with correct params when GetProductsByFileNameRepository throws

--- test/unit/data/usecase/remote.get.product.seeds.spec.ts
+++ test/unit/data/usecase/remote.get.product.seeds.spec.ts
@@ -1,5 +1,7 @@
+import { throwError } from "test/unit/domain/helper"
 import { GetFileNamesRepositorySpy, GetProductsByFileNameRepositorySpy, SetFileStatusRepositorySpy } from "../mock"
 import { RemoteGetProductSeeds } from "@/data/usecase"
+import { faker } from "@faker-js/faker"
 
 describe('RemoteGetProductSeeds', () => {
     test('should call GetProductsByFileNameRepository one time for each file name got', async () => {
@@ -16,5 +18,26 @@ describe('RemoteGetProductSeeds', () => {
         await sut.getSeeds()
 
         expect(namesCount).toBe(getProductsByFileNameRepositorySpy.count)
+    })
+
+    test('should call SetFileStatusRepository with correct params when GetProductsByFileNameRepository throws', async () => {
+        const getFileNamesRepositorySpy = new GetFileNamesRepositorySpy()
+        const getProductsByFileNameRepositorySpy = new GetProductsByFileNameRepositorySpy()
+        const setFileStatusRepositorySpy = new SetFileStatusRepositorySpy()
+        const sut = new RemoteGetProductSeeds(
+            getFileNamesRepositorySpy,
+            getProductsByFileNameRepositorySpy,
+            setFileStatusRepositorySpy
+        )
+        getFileNamesRepositorySpy.result = [faker.string.sample()]
+        jest.spyOn(getProductsByFileNameRepositorySpy, 'getProductsByFileName').mockImplementationOnce(throwError)
+        const firstNameFetched = getFileNamesRepositorySpy.result[0]
+        
+        await sut.getSeeds()
+
+        expect(setFileStatusRepositorySpy.params).toStrictEqual({
+            name: firstNameFetched,
+            status: 'error'
+        })
     })
 })
