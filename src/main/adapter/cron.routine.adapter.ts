b2874e582cde2f7cb33d98560969d4521209a719
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-08 18:12:17 -0300

    fix: set function as second param to enable execute routine correctly

--- src/main/adapter/cron.routine.adapter.ts
+++ src/main/adapter/cron.routine.adapter.ts
@@ -3,7 +3,7 @@ import cron from "node-cron"
 import { Routine } from "@/routine/protocol"
 
 export const adaptRoutine = (time: string, routine: Routine) => {
-    cron.schedule(time, routine.handle, {
+    cron.schedule(time, () => routine.handle(), {
         scheduled: true,
         timezone: "America/Sao_Paulo"
     })
