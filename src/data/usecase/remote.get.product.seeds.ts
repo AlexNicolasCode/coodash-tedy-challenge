5151523d7b9d5f28539da0d29937bfe3dbda3e74
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-08 16:08:04 -0300

    feat: RemoteGetProductSeeds usecase was created

---
+++ src/data/usecase/remote.get.product.seeds.ts
@@ -0,0 +1,33 @@
+import { Product } from "@/domain/model";
+import { GetProductSeeds } from "@/domain/usecase";
+import { GetFileNamesRepository, GetProductsByFileNameRepository } from "../protocol/http";
+import { SetFileStatusRepository } from "../protocol/db";
+
+export class RemoteGetProductSeeds implements GetProductSeeds {
+    constructor (
+        private readonly getFileNamesRepository: GetFileNamesRepository,
+        private readonly getProductsByFileNameRepository: GetProductsByFileNameRepository,
+        private readonly setFileStatusRepository: SetFileStatusRepository,
+    ) {}
+
+    async getSeeds (): Promise<Product[]> {
+        const fileNames = await this.getFileNamesRepository.getFileNames()
+        const products: Product[] = []
+        for (const name of fileNames) {
+            try {
+                const productsByName = await this.getProductsByFileNameRepository.getProductsByFileName(name)
+                products.concat(productsByName)
+                await this.setFileStatusRepository.setFileStatus({
+                    name: name,
+                    status: 'fetched'
+                })
+            } catch(error) {
+                await this.setFileStatusRepository.setFileStatus({
+                    name: name,
+                    status: 'error'
+                })
+            }
+        }
+        return products
+    }
+}
