19e3631653c25730c868bc2cd9253873ea93f8b9
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-08 15:52:47 -0300

    feat: add count param on GetProductsByFileNameRepository mock to enable know how many times it class was called

--- test/unit/data/mock/get.products.by.file.names.repository.spy.ts
+++ test/unit/data/mock/get.products.by.file.names.repository.spy.ts
@@ -4,10 +4,12 @@ import { mockProductList } from "test/unit/domain/mock";
 
 export class GetProductsByFileNameRepositorySpy implements GetProductsByFileNameRepository {
     name: string
+    count: number = 0
     result: Product[] = mockProductList()
 
     async getProductsByFileName (name: string): Promise<Product[]> {
         this.name = name
+        this.count++
         return this.result
     }
 }
