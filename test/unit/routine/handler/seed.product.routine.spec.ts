7bfeea8645317a8d3e84406e7b299a504e9df98f
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-09 13:10:20 -0300

    test: adapt SeedProductRoutine test suite to enable run them unit test correcly

--- test/unit/routine/handler/seed.product.routine.spec.ts
+++ test/unit/routine/handler/seed.product.routine.spec.ts
@@ -1,11 +1,12 @@
 import { SeedProductRoutine } from "@/routine/handler/seed.product.routine"
-import { BulkSaveProductSpy, GetProductSeedsSpy } from "../mock"
+import { BulkSaveProductSpy, GetProductSeedsSpy, SetRoutineStatusSpy } from "../mock"
 
 describe('SeedProductRoutine', () => {
     test('should call BulkSaveProduct with correct params', async () => {
+        const setRoutineStatusSpy = new SetRoutineStatusSpy()
         const getProductSeedsSpy = new GetProductSeedsSpy()
         const bulkSaveProductSpy = new BulkSaveProductSpy()
-        const sut = new SeedProductRoutine(getProductSeedsSpy, bulkSaveProductSpy)
+        const sut = new SeedProductRoutine(setRoutineStatusSpy, getProductSeedsSpy, bulkSaveProductSpy)
         
         await sut.handle()
 
