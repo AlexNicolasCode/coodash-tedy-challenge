4ddd6134f832b292eb4b67faef5ba4063814153c
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-09 12:16:41 -0300

    feat: SeedProductRoutine should use SetRoutineStatus to set when a routined to be runned, finished, or throws

--- src/routine/handler/seed.product.routine.ts
+++ src/routine/handler/seed.product.routine.ts
@@ -1,20 +1,33 @@
 import { Routine } from "../protocol";
-import { BulkSaveProduct, GetProductSeeds } from "@/domain/usecase";
+import { BulkSaveProduct, GetProductSeeds, SetRoutineStatus } from "@/domain/usecase";
 
 export class SeedProductRoutine implements Routine {
     constructor (
+        private readonly setRoutineStatus: SetRoutineStatus,
         private readonly getProductSeeds: GetProductSeeds,
         private readonly bulkSaveProduct: BulkSaveProduct
     ) {}
 
     async handle (): Promise<void> {
         try {
+            await this.setRoutineStatus.setStatus({
+                name: 'product-routine',
+                status: 'running'
+            })
             const products = await this.getProductSeeds.getSeeds()
             const hasSaved = await this.bulkSaveProduct.bulkSave(products)
             if (!hasSaved) {
                 throw new Error()
             }
+            this.setRoutineStatus.setStatus({
+                name: 'product-routine',
+                status: 'done'
+            })
         } catch (error) {
+            this.setRoutineStatus.setStatus({
+                name: 'product-routine',
+                status: 'error'
+            })
             console.log(error)
         }
     }
