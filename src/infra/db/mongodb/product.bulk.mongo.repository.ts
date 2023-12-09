bc0b5c5093c4c63c89e7fbb4b56b2316f339e17e
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-09 10:50:46 -0300

    feat: ProductBulkMongoRepository implements BulkSaveProductRepository and should be able to save product list when requested

--- src/infra/db/mongodb/product.bulk.mongo.repository.ts
+++ src/infra/db/mongodb/product.bulk.mongo.repository.ts
@@ -4,6 +4,29 @@ import { BulkSaveProductRepository } from "@/data/protocol/db";
 
 export class ProductBulkMongoRepository implements BulkSaveProductRepository {
     async bulkSave (products: Product[]): Promise<void> {
-        await ProductEntity.insertMany(products)
+        const codes = products.map((product) => product.code)
+        const productsFound = await ProductEntity.find({
+            code: {
+                $in: codes
+            }
+        })
+        const newProducts = products.filter((product) => 
+            !productsFound.some((productFound) => productFound.code == product.code)
+        )
+        const hasNewProducts = newProducts.length
+        if (hasNewProducts) {
+            await ProductEntity.insertMany(newProducts)
+        }
+        const productsToUpdate = products.filter((product) => 
+            productsFound.some((productFound) => 
+                productFound.code === product.code &&
+                productFound.last_modified_t < product.last_modified_t
+            )
+        )
+        for (const product of productsToUpdate) {
+            await ProductEntity.findOneAndUpdate({
+                code: product.code 
+            }, { ...product })
+        }
     }
 }
