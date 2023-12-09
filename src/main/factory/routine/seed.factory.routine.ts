191fe132013261b233dd40480999ca18b4cf6a5c
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-09 12:24:05 -0300

    feat: apply SetRoutineStatus factory on SeedProductRoutine factory to enable start SeedProductRoutine correctly when application start

--- src/main/factory/routine/seed.factory.routine.ts
+++ src/main/factory/routine/seed.factory.routine.ts
@@ -1,11 +1,13 @@
-import { makeBulkSaveProduct, makeGetProductSeeds } from "../usecase";
+import { makeBulkSaveProduct, makeGetProductSeeds, makeSetRoutineStatus } from "../usecase";
 import { Routine } from "@/routine/protocol";
 import { SeedProductRoutine } from "@/routine/handler";
 
 export const makeSeedProductRoutine = (): Routine => {
+    const setStatusRoutine = makeSetRoutineStatus()
     const getProductSeeds = makeGetProductSeeds()
     const bulkSaveProduct = makeBulkSaveProduct()
     return new SeedProductRoutine(
+        setStatusRoutine,
         getProductSeeds,
         bulkSaveProduct
     )
