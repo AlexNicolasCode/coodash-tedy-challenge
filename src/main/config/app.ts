43ddec36c4208859db3d4e7338562adc37955a2a
Alex Nicolas <alexoliveira604@gmail.com>
2023-12-09 16:06:44 -0300

    chore: express json was added to enable receive body parsed

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -4,6 +4,7 @@ import setupRoutes from "@/main/route"
 
 export const setupApp = async (): Promise<Express> => {
     const app = express()
+    app.use(express.json())
     setupRoutes(app)
     return app
   }
